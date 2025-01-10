9309c382617ebf55cc75adf4e662a6b7b72057cf
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-10 09:39:10 +0545

    django

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,4 +4,7 @@ from django.db import models
 # python manage.py makemigrations and python manage.py migrate (models.py change huda yo duita command chai run garnu parney)
 class Todo(models.Model):
     title = models.CharField(max_length=200)
-    
+
+    def __str__(self):
+     return self.title
+
