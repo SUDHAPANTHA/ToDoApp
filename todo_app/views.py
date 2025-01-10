12a0a5804959a64ff411844aff34b90d73a51318
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-10 10:26:00 +0545

    create and delete

--- todo_app/views.py
+++ todo_app/views.py
@@ -7,8 +7,16 @@ def todo_list(request):
     todos = Todo.objects.all()
     return render(request,"todo_list.html",{"todos": todos})
     
-def todo_delete(request, id):
-    todo = Todo.objects.get(id = id)
+def todo_delete(request, pk):
+    todo = Todo.objects.get(id=pk)
     todo.delete()
     return HttpResponseRedirect("/")
+def todo_create(request):
+    if request.method == "GET":
+        return render(request,"todo_create.html")
+    else:
+        title = request.POST["title"]
+        todo = Todo(title=title)
+        todo.save
+        return HttpResponseRedirect("/")
     
