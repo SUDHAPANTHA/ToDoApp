f0a14d3c1faf3803ebd00cb8f7ff116403274241
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-13 21:43:47 +0545

    change on ui

---
+++ todo_app/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from todo_app import views
+
+urlpatterns = [
+    path("", views.todo_list),
+    path("delete/<int:pk>/", views.todo_delete),
+    path("create/", views.todo_create),
+    path("update/<int:id>/", views.todo_update),
+
+]
